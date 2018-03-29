2745227a1b6b5589f2a3c9e8a1c3c61a7c0c3289
Thorax <thorax@me.com>
2018-03-29 17:18:08 +0300

    Initial Commit

---
+++ BigNumbers/BigNumbers.h
@@ -0,0 +1,19 @@
+//
+//  BigNumbers.h
+//  BigNumbers
+//
+//  Created by Yaroslav Pasternak on 29.03.2018.
+//  Copyright © 2018 Teambrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BigNumbers.
+FOUNDATION_EXPORT double BigNumbersVersionNumber;
+
+//! Project version string for BigNumbers.
+FOUNDATION_EXPORT const unsigned char BigNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigNumbers/PublicHeader.h>
+
+
